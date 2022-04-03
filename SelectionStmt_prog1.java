d8ca0da562e0127c736094fad1387a400feb4ba1
gotoharjeet <gotoharjeet@gmail.com>
2022-04-03 18:38:14 +0530

    if-else problems

---
+++ SelectionStmt_prog1.java
@@ -0,0 +1,111 @@
+import java.util.Scanner;
+class  SelectionStmt_prog1
+{
+	public static void main(String[] args) 
+	{
+	//First Problem
+
+	/*System.out.println("int x=0");
+
+	System.out.println("if(x)");
+	System.out.println("{");
+    String syso1="System.out.println("+"Hello"+")";
+	System.out.println(syso1);	
+    System.out.println("}");
+	System.out.println("else");							
+	System.out.println("{");				
+    String syso2="System.out.println("+"Hi"+")";
+	System.out.println(syso2);
+	System.out.println("}");	
+
+	System.out.println("A:Hello   B:Hi   C:CE   D:RE");
+	Scanner obj=new Scanner(System.in);
+	String option=obj.nextLine();
+	System.out.println("Selected Option :"+option);*/
+	boolean b=false;
+	if(b==false)
+	{
+	  System.out.println("Hello");
+	}
+	else
+	{
+		System.out.println("Hi");
+	}				
+	
+
+	//SECOND PROBLEM
+	/*System.out.println("int x=10");
+
+	System.out.println("if(x=20)");
+	System.out.println("{");
+    String syso1="System.out.println("+"Hello"+")";
+	System.out.println(syso1);	
+    System.out.println("}");
+	System.out.println("else");							
+	System.out.println("{");				
+    String syso2="System.out.println("+"Hi"+")";
+	System.out.println(syso2);
+	System.out.println("}");	
+
+	System.out.println("A:Hello   B:Hi   C:CE   D:RE");
+	Scanner obj=new Scanner(System.in);
+	String option=obj.nextLine();
+	System.out.println("Selected Option :"+option);*/
+
+	//THIRD PROBLEM
+	/*System.out.println("int x=10");
+
+	System.out.println("if(x==10)");
+	System.out.println("{");
+    String syso1="System.out.println("+"Hello"+")";
+	System.out.println(syso1);	
+    System.out.println("}");
+	System.out.println("else");							
+	System.out.println("{");				
+    String syso2="System.out.println("+"Hi"+")";
+	System.out.println(syso2);
+	System.out.println("}");	
+
+	System.out.println("A:Hello   B:Hi   C:CE   D:RE");
+	Scanner obj=new Scanner(System.in);
+	String option=obj.nextLine();
+	System.out.println("Selected Option :"+option);*/
+
+	//FOURTH PROBLEM
+	/*System.out.println("Boolean b=true");
+
+	System.out.println("if(b=false)");
+	System.out.println("{");
+    String syso1="System.out.println("+"Hello"+")";
+	System.out.println(syso1);	
+    System.out.println("}");
+	System.out.println("else");							
+	System.out.println("{");				
+    String syso2="System.out.println("+"Hi"+")";
+	System.out.println(syso2);
+	System.out.println("}");	
+
+	System.out.println("A:Hello   B:Hi   C:CE   D:RE");
+	Scanner obj=new Scanner(System.in);
+	String option=obj.nextLine();
+	System.out.println("Selected Option :"+option);*/
+	//FIFTH PROBLEM
+	/*System.out.println("boolean b=true");
+
+	System.out.println("if(b==false)");
+	System.out.println("{");
+    String syso1="System.out.println("+"Hello"+")";
+	System.out.println(syso1);	
+    System.out.println("}");
+	System.out.println("else");							
+	System.out.println("{");				
+    String syso2="System.out.println("+"Hi"+")";
+	System.out.println(syso2);
+	System.out.println("}");	
+
+	System.out.println("A:Hello   B:Hi   C:CE   D:RE");
+	Scanner obj=new Scanner(System.in);
+	String option=obj.nextLine();
+	System.out.println("Selected Option :"+option);*/
+	}
+}
